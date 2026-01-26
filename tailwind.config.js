783ec14f507ea5476d02036e92725f7ab229952a
atibon2988 <luuminhchau2997@gmail.com>
2026-01-26 17:33:07 +0700

    tesst

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  darkMode: 'class', // <--- Dòng này quan trọng nhất để bật Dark Mode
+/** @type {import("tailwindcss").Config} */
+export default {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,4 +11,3 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
