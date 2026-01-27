43fd1bbb10d2bbfab0d699348ed75f7690deb3e5
atibon2988 <luuminhchau2997@gmail.com>
2026-01-27 10:04:04 +0700

    dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-/** @type {import("tailwindcss").Config} */
-export default {
-  darkMode: "class",
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // <--- DÒNG QUAN TRỌNG NHẤT
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
